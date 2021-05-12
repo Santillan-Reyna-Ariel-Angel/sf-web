c79b153101c393040e121aaccd739fa259501a29
Santillan Reyna Ariel Angel <santillanreynaarielangel1@gmail.com>
2021-05-11 20:37:10 -0400

    REACT_Y_TAILWINDCSS_CONFIGURADOS_LISTO_PARA_USAR

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1>tailwindCss Con React</h1>
+            <h2>Configurado y listo para utilizar</h2>
+        </div>
+    )
+}
+
+export default App
